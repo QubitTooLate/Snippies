601fc212b39b86a337a5c398b1c37b8cbd55981d
Jester <58484964+QubitTooLate@users.noreply.github.com>
2022-12-06 19:59:42 +0100

    Efficiency mode guide update

--- EfficiencyMode.cs
+++ EfficiencyMode.cs
@@ -7,6 +7,8 @@
 
 // Error checking is removed from this example!
 
+using System.Runtime.InteropServices;
+
 const uint PROCESS_SET_INFORMATION = 0x0200;
 const uint IDLE_PRIORITY_CLASS = 0x00000040;
 const uint ProcessPowerThrottling = 4;
@@ -15,23 +17,23 @@ const uint PROCESS_POWER_THROTTLING_EXECUTION_SPEED = 1;
 
 // Get our process handle
 int id = GetCurrentProcessId();
-IntPtr processHandle = OpenProcess(PROCESS_SET_INFORMATION, false, id));
+IntPtr processHandle = OpenProcess(PROCESS_SET_INFORMATION, 0, id);
 
 // To set the priority of the process
 SetPriorityClass(processHandle, IDLE_PRIORITY_CLASS);
 
 // To set efficiency mode
-PROCESS_POWER_THROTTLING_STATE state =
+PROCESS_POWER_THROTTLING_STATE state = new()
 {
-	ControlMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
-	StateMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
-	Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION,
+    ControlMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+    StateMask = PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+    Version = PROCESS_POWER_THROTTLING_CURRENT_VERSION,
 };
 
-SetProcessInformation(processHandle, ProcessPowerThrottling, &state, sizeof(PROCESS_POWER_THROTTLING_STATE)));
+unsafe { SetProcessInformation(processHandle, ProcessPowerThrottling, &state, sizeof(PROCESS_POWER_THROTTLING_STATE)); }
 
 // To set the cores to run on
-SetProcessAffinityMask(processHandle, 1);
+SetProcessAffinityMask(processHandle, 1); // In this case only core 0
 
 //Don't forget to clean up after yourself!
 CloseHandle(processHandle);
@@ -39,7 +41,7 @@ CloseHandle(processHandle);
 return;
 
 // https://devblogs.microsoft.com/performance-diagnostics/introducing-ecoqos/
-// https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessid
+// https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocessid
 // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
 // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setpriorityclass
 // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-process_power_throttling_state
@@ -48,58 +50,53 @@ return;
 
 [DllImport("kernel32", SetLastError = true)]
 [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-[return: MarshalAs(UnmanagedType.Bool)]
-static extern bool SetPriorityClass(
-  IntPtr hProcess,
-  uint dwPriorityClass
-);
-
-[DllImport("kernel32", SetLastError = true)]
-[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-static extern uint GetPriorityClass(
-  IntPtr hProcess
-);
+static extern int GetCurrentProcessId();
 
 [DllImport("kernel32", SetLastError = true)]
 [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
 static extern IntPtr OpenProcess(
-  uint dwDesiredAccess,
-  byte bInheritHandle,
-  int dwProcessId
+    uint dwDesiredAccess,
+    byte bInheritHandle,
+    int dwProcessId
+);
+
+[DllImport("kernel32", SetLastError = true)]
+[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern bool SetPriorityClass(
+    IntPtr hProcess,
+    uint dwPriorityClass
+);
+
+[DllImport("kernel32", SetLastError = true)]
+[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern unsafe bool SetProcessInformation(
+    IntPtr hProcess,
+      uint ProcessInformationClass,
+    void* ProcessInformation,
+    int ProcessInformationSize
+);
+
+[DllImport("kernel32", SetLastError = true)]
+[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+[return: MarshalAs(UnmanagedType.Bool)]
+static extern bool SetProcessAffinityMask(
+    IntPtr hProcess,
+    nuint dwProcessAffinityMask
 );
 
 [DllImport("kernel32", SetLastError = true)]
 [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
 [return: MarshalAs(UnmanagedType.Bool)]
 static extern bool CloseHandle(
-	IntPtr hObject
-);
-
-
-[DllImport("kernel32", SetLastError = true)]
-[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-[return: MarshalAs(UnmanagedType.Bool)]
-static extern unsafe bool SetProcessInformation(
-	IntPtr hProcess,
-  	uint ProcessInformationClass,
-	void* ProcessInformation,
-	int ProcessInformationSize
-);
-
-[DllImport("kernel32", SetLastError = true)]
-[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-[return: MarshalAs(UnmanagedType.Bool)]
-static extern unsafe bool GetProcessInformation(
-	IntPtr hProcess,
-	uint ProcessInformationClass,
-	void* ProcessInformation,
-	int ProcessInformationSize
+    IntPtr hObject
 );
 
 [StructLayout(LayoutKind.Sequential)]
 struct PROCESS_POWER_THROTTLING_STATE
 {
-	public uint Version;
-	public uint ControlMask;
-	public uint StateMask;
+    public uint Version;
+    public uint ControlMask;
+    public uint StateMask;
 }
